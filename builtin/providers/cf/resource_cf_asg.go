8dff7a139802563cc3de1d1419a8f30d2027e26d
Mevan Samaratunga <mevansam@gmail.com>
2017-06-14 09:26:45 -0700

    added ICMP rules and Description arguments to cf_asg resource [Finished #147143529] [Finished #147143475]

--- builtin/providers/cf/resource_cf_asg.go
+++ builtin/providers/cf/resource_cf_asg.go
@@ -28,8 +28,9 @@ func resourceAsg() *schema.Resource {
 				Elem: &schema.Resource{
 					Schema: map[string]*schema.Schema{
 						"protocol": &schema.Schema{
-							Type:     schema.TypeString,
-							Required: true,
+							Type:         schema.TypeString,
+							Required:     true,
+							ValidateFunc: validateAsgProtocol,
 						},
 						"destination": &schema.Schema{
 							Type:     schema.TypeString,
@@ -39,16 +40,38 @@ func resourceAsg() *schema.Resource {
 							Type:     schema.TypeString,
 							Optional: true,
 						},
+						"type": &schema.Schema{
+							Type:     schema.TypeInt,
+							Optional: true,
+							Default:  0,
+						},
+						"code": &schema.Schema{
+							Type:     schema.TypeInt,
+							Optional: true,
+							Default:  0,
+						},
 						"log": &schema.Schema{
 							Type:     schema.TypeBool,
 							Optional: true,
 							Default:  false,
+						},
+						"description": &schema.Schema{
+							Type:     schema.TypeString,
+							Optional: true,
 						},
 					},
 				},
 			},
 		},
 	}
+}
+
+func validateAsgProtocol(v interface{}, k string) (ws []string, errs []error) {
+	value := v.(string)
+	if value != "tcp" && value != "icmp" && value != "udp" && value != "all" {
+		errs = append(errs, fmt.Errorf("%q must be one of 'tcp', 'icmp', 'udp' or 'all'", k))
+	}
+	return
 }
 
 func resourceAsgCreate(d *schema.ResourceData, meta interface{}) error {
@@ -59,7 +82,12 @@ func resourceAsgCreate(d *schema.ResourceData, meta interface{}) error {
 	}
 
 	am := session.ASGManager()
-	id, err := am.CreateASG(d.Get("name").(string), readASGRulesFromConfig(d))
+
+	rules, err := readASGRulesFromConfig(d)
+	if err != nil {
+		return err
+	}
+	id, err := am.CreateASG(d.Get("name").(string), rules)
 	if err != nil {
 		return err
 	}
@@ -93,7 +121,12 @@ func resourceAsgRead(d *schema.ResourceData, meta interface{}) error {
 		if len(r.Ports) > 0 {
 			tfRule["ports"] = r.Ports
 		}
+		if r.Protocol == "icmp" {
+			tfRule["type"] = r.Type
+			tfRule["code"] = r.Code
+		}
 		tfRule["log"] = r.Log
+		tfRule["description"] = r.Description
 		tfRules = append(tfRules, tfRule)
 	}
 	d.Set("rule", tfRules)
@@ -109,7 +142,12 @@ func resourceAsgUpdate(d *schema.ResourceData, meta interface{}) error {
 	}
 
 	am := session.ASGManager()
-	err := am.UpdateASG(d.Id(), d.Get("name").(string), readASGRulesFromConfig(d))
+
+	rules, err := readASGRulesFromConfig(d)
+	if err != nil {
+		return err
+	}
+	err = am.UpdateASG(d.Id(), d.Get("name").(string), rules)
 	if err != nil {
 		return err
 	}
@@ -125,7 +163,7 @@ func resourceAsgDelete(d *schema.ResourceData, meta interface{}) error {
 	return session.ASGManager().Delete(d.Id())
 }
 
-func readASGRulesFromConfig(d *schema.ResourceData) (rules []cfapi.CCASGRule) {
+func readASGRulesFromConfig(d *schema.ResourceData) (rules []cfapi.CCASGRule, err error) {
 
 	rules = []cfapi.CCASGRule{}
 	for _, r := range d.Get("rule").([]interface{}) {
@@ -138,9 +176,26 @@ func readASGRulesFromConfig(d *schema.ResourceData) (rules []cfapi.CCASGRule) {
 		if v, ok := tfRule["ports"]; ok {
 			asgRule.Ports = v.(string)
 		}
+		if v, ok := tfRule["type"]; ok {
+			asgRule.Type = v.(int)
+		}
+		if v, ok := tfRule["code"]; ok {
+			asgRule.Code = v.(int)
+		}
 		if v, ok := tfRule["log"]; ok {
 			asgRule.Log = v.(bool)
 		}
+		if v, ok := tfRule["description"]; ok {
+			asgRule.Description = v.(string)
+		}
+
+		if asgRule.Protocol != "icmp" && (asgRule.Type > 0 || asgRule.Code > 0) {
+			err = fmt.Errorf(
+				"'type' or 'code' arguments are valid only for 'icmp' protocol and not for '%s' protocol",
+				asgRule.Protocol)
+			return
+		}
+
 		rules = append(rules, asgRule)
 	}
 	return
